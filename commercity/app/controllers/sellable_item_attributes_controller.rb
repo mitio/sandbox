fc68315432518fd919cca599c019199d1d47a093
Dimitar Dimitrov <wireman@gmail.com>
2011-09-20 18:57:33 +0300

    Remove unused respond_to formats in SellableItemAttributesController.

--- commercity/app/controllers/sellable_item_attributes_controller.rb
+++ commercity/app/controllers/sellable_item_attributes_controller.rb
@@ -1,57 +1,29 @@
 class SellableItemAttributesController < ApplicationController
   before_filter :set_sellable_item
 
-  # GET /sellable_item_attributes
-  # GET /sellable_item_attributes.json
   def index
     @sellable_item_attributes = @sellable_item.sellable_item_attributes
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @sellable_item_attributes }
-    end
   end
 
-  # GET /sellable_item_attributes/1
-  # GET /sellable_item_attributes/1.json
   def show
     @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @sellable_item_attribute }
-    end
   end
 
-  # GET /sellable_item_attributes/new
-  # GET /sellable_item_attributes/new.json
   def new
     @sellable_item_attribute = @sellable_item.sellable_item_attributes.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @sellable_item_attribute }
-    end
   end
 
-  # GET /sellable_item_attributes/1/edit
   def edit
     @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
   end
 
-  # POST /sellable_item_attributes
-  # POST /sellable_item_attributes.json
   def create
     @sellable_item_attribute = @sellable_item.sellable_item_attributes.build(params[:sellable_item_attribute])
 
-    respond_to do |format|
-      if @sellable_item_attribute.save
-        format.html { redirect_to [@sellable_item, @sellable_item_attribute], notice: 'Sellable item attribute was successfully created.' }
-        format.json { render json: @sellable_item_attribute, status: :created, location: @sellable_item_attribute }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @sellable_item_attribute.errors, status: :unprocessable_entity }
-      end
+    if @sellable_item_attribute.save
+      redirect_to [@sellable_item, @sellable_item_attribute], notice: 'Sellable item attribute was successfully created.'
+    else
+      render action: "new"
     end
   end
 
@@ -60,14 +32,10 @@ class SellableItemAttributesController < ApplicationController
   def update
     @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
 
-    respond_to do |format|
-      if @sellable_item_attribute.update_attributes(params[:sellable_item_attribute])
-        format.html { redirect_to [@sellable_item, @sellable_item_attribute], notice: 'Sellable item attribute was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @sellable_item_attribute.errors, status: :unprocessable_entity }
-      end
+    if @sellable_item_attribute.update_attributes(params[:sellable_item_attribute])
+      redirect_to [@sellable_item, @sellable_item_attribute], notice: 'Sellable item attribute was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
@@ -77,10 +45,7 @@ class SellableItemAttributesController < ApplicationController
     @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
     @sellable_item_attribute.destroy
 
-    respond_to do |format|
-      format.html { redirect_to sellable_item_attributes_url }
-      format.json { head :ok }
-    end
+    redirect_to sellable_item_attributes_url
   end
 
   protected
