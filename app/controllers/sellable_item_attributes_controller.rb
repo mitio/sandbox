df0d2841f498625249b38aaa3ba9679b57ed7b86
Dimitar Dimitrov <wireman@gmail.com>
2011-09-20 18:41:13 +0300

    Scope all operations on SellableItemAttribute to @sellable_item.
    Enhances security and readability and ensures we adhere to the "Rails"-way.

--- app/controllers/sellable_item_attributes_controller.rb
+++ app/controllers/sellable_item_attributes_controller.rb
@@ -4,7 +4,7 @@ class SellableItemAttributesController < ApplicationController
   # GET /sellable_item_attributes
   # GET /sellable_item_attributes.json
   def index
-    @sellable_item_attributes = SellableItemAttribute.all(:sellable_item_id => params[:sellable_item_id])
+    @sellable_item_attributes = @sellable_item.sellable_item_attributes
 
     respond_to do |format|
       format.html # index.html.erb
@@ -15,7 +15,7 @@ class SellableItemAttributesController < ApplicationController
   # GET /sellable_item_attributes/1
   # GET /sellable_item_attributes/1.json
   def show
-    @sellable_item_attribute = SellableItemAttribute.find(params[:id])
+    @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -26,7 +26,7 @@ class SellableItemAttributesController < ApplicationController
   # GET /sellable_item_attributes/new
   # GET /sellable_item_attributes/new.json
   def new
-    @sellable_item_attribute = SellableItemAttribute.new
+    @sellable_item_attribute = @sellable_item.sellable_item_attributes.build
 
     respond_to do |format|
       format.html # new.html.erb
@@ -36,13 +36,13 @@ class SellableItemAttributesController < ApplicationController
 
   # GET /sellable_item_attributes/1/edit
   def edit
-    @sellable_item_attribute = SellableItemAttribute.find(params[:id])
+    @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
   end
 
   # POST /sellable_item_attributes
   # POST /sellable_item_attributes.json
   def create
-    @sellable_item_attribute = SellableItemAttribute.new(params[:sellable_item_attribute])
+    @sellable_item_attribute = @sellable_item.sellable_item_attributes.build(params[:sellable_item_attribute])
 
     respond_to do |format|
       if @sellable_item_attribute.save
@@ -58,7 +58,7 @@ class SellableItemAttributesController < ApplicationController
   # PUT /sellable_item_attributes/1
   # PUT /sellable_item_attributes/1.json
   def update
-    @sellable_item_attribute = SellableItemAttribute.find(params[:id])
+    @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
 
     respond_to do |format|
       if @sellable_item_attribute.update_attributes(params[:sellable_item_attribute])
@@ -74,7 +74,7 @@ class SellableItemAttributesController < ApplicationController
   # DELETE /sellable_item_attributes/1
   # DELETE /sellable_item_attributes/1.json
   def destroy
-    @sellable_item_attribute = SellableItemAttribute.find(params[:id])
+    @sellable_item_attribute = @sellable_item.sellable_item_attributes.find(params[:id])
     @sellable_item_attribute.destroy
 
     respond_to do |format|
